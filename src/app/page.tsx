4222b53e59108603778c1b5822b83553cfea4230
mohameddn988 <msideneche@gmail.com>
2026-01-28 01:31:27 +0100

    Finnished the Header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import HeroSection from "@/components/sections/HeroSection";
+
 export default function Home() {
   return (
-    <div>
-
-    </div>
+    <main>
+      <HeroSection />
+      <HeroSection />
+    </main>
   );
 }
